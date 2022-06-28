ac38e18572a1390d6a78587780cefbb31d450120
nazmulnabil <nabilnazmul868@gmail.com>
2022-06-28 12:22:16 +0600

    pages created
    list page + details page

---
+++ lib/modules/repo_details_page/presentation/views/pages/repo_details_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RepoDetailsPage extends StatelessWidget {
+  const RepoDetailsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
